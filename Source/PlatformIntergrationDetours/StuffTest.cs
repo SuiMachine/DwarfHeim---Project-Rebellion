6219b12a528312b47d03887ca6d7a17b918d7b48
SuiMachine <SuiMachine@users.noreply.github.com>
2023-12-10 14:45:57 +0100

    Removed empty junk

---
+++ Source/PlatformIntergrationDetours/StuffTest.cs
@@ -0,0 +1,12 @@
+﻿using DwarfHeim.PlatformUserIntegration;
+using DwarfHeim.SaveGame;
+using DwarfHeim.UI.MainMenu;
+using HarmonyLib;
+using PineCone.Networking.Client;
+
+namespace ProjectRebellion.PlatformIntergrationDetours
+{
+    public static class StuffTest
+    {
+	}
+}
