548b78d3df221f7a494e4db69427c54c47a3fec6
SuiMachine <SuiMachine@users.noreply.github.com>
2023-12-16 23:41:41 +0100

    Disable leaderboard tab, cause we will never get it working without Steamworks admin access

---
+++ Source/MainMenu/MenuNavigationHook.cs
@@ -0,0 +1,24 @@
+﻿using DwarfHeim.UI.MainMenu;
+using HarmonyLib;
+
+namespace ProjectRebellion.MainMenu
+{
+	[HarmonyPatch]
+	public static class MenuNavigationHook
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(MenuNavigation), "Start")]
+		public static void PostfixStart(MenuNavigation __instance)
+		{
+			if (__instance.gameObject.name == "Profile")
+			{
+				var find = __instance.transform.Find("Panel/NavBar/Leaderboard");
+				if (find != null)
+				{
+					//We will never be able to recreate leaderboards, unless somehow we get access to Steamworks itself and active them there
+					find.gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+}
